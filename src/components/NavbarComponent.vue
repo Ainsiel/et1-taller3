9fe7ae51898cdda88665dc6a786dc638f26b20b8
Cristóbal Pavez <c.pavez07@ufromail.cl>
2024-05-29 17:32:22 -0400

    NavbarComponent updated.

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="d-flex w-full">
-        <h1>Taller 3</h1>
-        <router-link to="/">Wiki</router-link>
-        <router-link to="/game">Game</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Taller 3</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link class="nav-link active" aria-current="page" to="/">Wiki</router-link>
+                    <router-link class="nav-link active" aria-current="page" to="/game">Game</router-link>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script setup>
