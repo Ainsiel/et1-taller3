fcb27b189363fc4bb01968d4193b2fd1e8e972ab
Mahiiru <c.pavez07@ufromail.cl>
2024-06-07 09:49:45 -0400

    Fix RecordCard gifs

--- src/components/RecordCard.vue
+++ src/components/RecordCard.vue
@@ -2,7 +2,7 @@
     <div class="card mb-3">
         <div class="card-body d-flex justify-content-between align-items-center">
             <div class="text-center" v-if="props.character1" >
-                <img :src="`../assets/${props.character1}/idle.gif`" class="character-img" alt="Character 1">
+                <img :src="`src/assets/${props.character1}/idle.gif`" class="character-img" alt="Character 1">
                 <h5 class="card-title">{{ props.username1 }}</h5>
                 <p class="card-text" :class="{ 'text-success': props.u1Won, 'text-danger': !props.u1Won }">
                     {{ props.u1Won ? 'GANO' : 'PERDIO' }}
@@ -12,7 +12,7 @@
                 <h3>VS</h3>
             </div>
             <div class="text-center" v-if="props.character2" >
-                <img :src="`../assets/${props.character2}/idle.gif`" class="character-img" alt="Character 2">
+                <img :src="`src/assets/${props.character2}/idle.gif`" class="character-img" alt="Character 2">
                 <h5 class="card-title">{{ props.username2 }}</h5>
                 <p class="card-text" :class="{ 'text-success': props.u2Won, 'text-danger': !props.u2Won }">
                     {{ props.u2Won ? 'GANO' : 'PERDIO' }}
