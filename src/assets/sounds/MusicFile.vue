89526f33db5f1d764aef350fb0b34f9903241c6f
Ignacio Manríquez <i.manriquez01@ufromail.cl>
2024-06-07 13:34:45 -0400

    Add Music Files
    2 Music
    2 Sounds effects

---
+++ src/assets/sounds/MusicFile.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- Tu contenido actual -->
+    <audio autoplay loop>
+      <source :src="musicSrc" type="audio/mp3">
+      Tu navegador no soporta la reproducción de audio.
+    </audio>
+    <!-- Resto de tu contenido -->
+  </template>
+  
+  <script setup>
+  // Importa el archivo de música
+  const musicSrc = '/src/assets/sounds/medieval-star-188280.mp3';
+  </script>
